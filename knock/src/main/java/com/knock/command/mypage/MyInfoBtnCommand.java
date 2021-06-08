54d327a37f31a743b10f7db66ca84913dc79a2c0
hyemin <hhm930415@gmail.com>
2021-06-08 18:51:13 +0900

    [4] 마이페이지 내정보 보기

---
+++ knock/src/main/java/com/knock/command/mypage/MyInfoBtnCommand.java
@@ -0,0 +1,25 @@
+package com.knock.command.mypage;
+
+import java.io.IOException; 
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.knock.command.Command;
+import com.knock.dao.UserDAO;
+import com.knock.vo.UserVO;
+
+public class MyInfoBtnCommand implements Command{
+
+	@Override
+	public String exec(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		
+		UserVO vo = UserDAO.myPageList(id);
+		request.setAttribute("vo", vo);
+		
+		return "00_mypage/updateMypage.jsp";
+	}
+	
+}
