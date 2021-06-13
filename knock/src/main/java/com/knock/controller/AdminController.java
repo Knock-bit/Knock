dc2ce4ac4a4234736123b9fb0719b2a274a91171
HanJeongSeol <hanjeong94@naver.com>
2021-06-13 15:38:57 +0900

    [8] 키워드추가 - 페이징 오류 미해결

--- knock/src/main/java/com/knock/controller/AdminController.java
+++ knock/src/main/java/com/knock/controller/AdminController.java
@@ -1,6 +1,9 @@
 package com.knock.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,11 +25,18 @@ import com.knock.model.command.admin.AdminUserListCommand;
 import com.knock.model.command.campaign.CampaignListCommand;
 import com.knock.model.command.campaign.CampaignOneCommand;
 import com.knock.model.command.campaign.ProposalCommand;
+import com.knock.model.dao.AdminKeywordDAO;
 
 @WebServlet("/admin")
 public class AdminController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	
 
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doHandle(request, response);
@@ -42,13 +52,13 @@ public class AdminController extends HttpServlet {
 			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html; charset=utf-8");
-
+		
 		String action = request.getPathInfo();
 		System.out.println("action:" + action);
 
 		String type = request.getParameter("type");
 		System.out.println("type: " + type);
-
+		
 		Command command = null;
 		if ("/index.jsp".equals(action)) {
 		} else if ("adminUserList".equals(type)) {
@@ -56,10 +66,13 @@ public class AdminController extends HttpServlet {
 		} else if ("active".equals(type)) {
 			command = new AdminUserActiveUpdate();
 		} else if ("adminKeyword".equals(type)) {
+			System.out.println("cpage: " + request.getParameter("cPage"));
 			command = new AdminKeywordList();
 		} else if ("adminKeywordDel".equals(type)) {
 			command = new AdminKeywordDel();
-			System.out.println("keydelcommand : " + command);
+		} else if("adminKeywordAdd".equals(type)) {
+//			String result = AdminKeywordAjax.keyAdd(request, response);
+			command = new AdminKeywordAdd();
 		}
 		if (command == null || command.equals("")) {
 			System.out.println("command가 빈칸이거나 null임");
---
+++ knock/src/main/java/com/knock/controller/AdminKeyController.java
@@ -0,0 +1,45 @@
+package com.knock.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.filters.SetCharacterEncodingFilter;
+
+/**
+ * Servlet implementation class AdminKeyController
+ */
+@WebServlet("/admin/keyword/*")
+public class AdminKeyController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public AdminKeyController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doHandle(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doHandle(request, response);
+	}
+
+	private void doHandle(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		String action = request.getPathInfo();
+		System.out.println(action);
+
+	}
+
+}
